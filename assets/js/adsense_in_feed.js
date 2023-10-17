d65bdfa8052beba0abbc800418eccbc9440a57c0
devmjun <dev.mjun@gmail.com>
2023-10-17 21:53:06 +0900

    home url 오류 수정

--- assets/js/adsense_in_feed.js
+++ assets/js/adsense_in_feed.js
@@ -2,13 +2,13 @@
 
 // blog-post 내에 AdSense in article 코드 삽입.
 function insertAdInRandomLocation() {
-    var adCode = 
+    let adCode = 
     '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1101850893094860" crossorigin="anonymous"><\/script>' +
     '<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-format="fluid" data-ad-layout-key="-fb+5w+4e-db+86" data-ad-client="ca-pub-1101850893094860" data-ad-slot="3903322695"><\/ins>' + 
     '<script> (adsbygoogle = window.adsbygoogle || []).push({}); <\/script>';
 
     var adElement = document.createElement("div");
-    adElement.innerHTML = adCode    
+    adElement.innerHTML = adCode;    
 
     var elements = document.querySelectorAll('.post-preview');
 
