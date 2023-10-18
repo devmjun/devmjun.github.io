2108c60f52c5b2540830d687271d25e9c43cfe25
devmjun <dev.mjun@gmail.com>
2023-10-18 12:24:33 +0900

    fix adsense in feed errors

--- assets/js/adsense_in_feed.js
+++ assets/js/adsense_in_feed.js
@@ -3,20 +3,24 @@
 // blog-post 내에 AdSense in article 코드 삽입.
 function insertAdInRandomLocation() {
     let adCode = 
-    '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1101850893094860" crossorigin="anonymous"><\/script>' +
-    '<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-format="fluid" data-ad-layout-key="-fb+5w+4e-db+86" data-ad-client="ca-pub-1101850893094860" data-ad-slot="3903322695"><\/ins>' + 
-    '<script> (adsbygoogle = window.adsbygoogle || []).push({}); <\/script>';
+    '<li class="post-preview adsense">' +
+    '<article>' +
+    '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1101850893094860" crossorigin="anonymous"><\/script>'+
+    '<ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-d2-a+73-vo+fv" data-ad-client="ca-pub-1101850893094860" data-ad-slot="2708788380"><\/ins>' +
+    '<script> (adsbygoogle = window.adsbygoogle || []).push({}); <\/script>'+
+    '<\/article>' +
+    '<\/li>' ; 
 
-    var adElement = document.createElement("div");
-    adElement.innerHTML = adCode;    
+    // var adElement = document.createElement("div");
+    // adElement.innerHTML = adCode;    
 
     var elements = document.querySelectorAll('.post-preview');
 
     var randomIndex = Math.floor(Math.random() * elements.length);
     var randomElement = elements[randomIndex];
     
-    // 광고 코드를 삽입
-    randomElement.insertBefore(adElement, randomElement.firstChild);
+    // randomElement.insertBefore(adElement, randomElement.firstChild);
+    randomElement.insertAdjacentHTML('beforebegin', adCode)
 }
 
 document.addEventListener("DOMContentLoaded", insertAdInRandomLocation);
