279a666ba42da8c9ef52420a77584d67afba7ab1
devmjun <dev.mjun@gmail.com>
2023-10-18 13:10:32 +0900

    add .ja for in feed adsense

--- assets/js/adsense_in_feed.js
+++ assets/js/adsense_in_feed.js
@@ -3,7 +3,8 @@
 // blog-post 내에 AdSense in article 코드 삽입.
 function insertAdInRandomLocation() {
     let adCode = 
-    '<li class="post-preview adsense">' +
+    // '<style> .fixed-height { height: 1000px; } <\/style>' +
+    '<li class="post-preview fixed-height" id="ad-container">' +
     '<article>' +
     '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1101850893094860" crossorigin="anonymous"><\/script>'+
     '<ins class="adsbygoogle" style="display:block" data-ad-format="fluid" data-ad-layout-key="-d2-a+73-vo+fv" data-ad-client="ca-pub-1101850893094860" data-ad-slot="2708788380"><\/ins>' +
@@ -20,7 +21,12 @@ function insertAdInRandomLocation() {
     var randomElement = elements[randomIndex];
     
     // randomElement.insertBefore(adElement, randomElement.firstChild);
-    randomElement.insertAdjacentHTML('beforebegin', adCode)
+    randomElement.insertAdjacentHTML('beforebegin', adCode);
+
+    var adContainer = document.getElementById("ad-container");
+    let randomContainerHeight = randomElement.clientHeight;
+    adContainer.style.height =  randomContainerHeight + 'px';
+    // adContainer.style.height = 1000;
 }
 
 document.addEventListener("DOMContentLoaded", insertAdInRandomLocation);
